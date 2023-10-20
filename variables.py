3d245c29c92e0401eae6cc707531126fae64b50e
mesupreneur <ma.mesupadhikari@gmail.com>
2023-10-20 18:08:30 +0545

    Variables in Python

---
+++ variables.py
@@ -0,0 +1,11 @@
+name = 'Mesup'
+age = 18
+#we can't use + incase of integer like age=18
+#, can be used for both case concatenate
+print(name + ' is a boy')
+print(name + ' is ',age)
+print(name + ' is from Nepal')
+
+print(name)
+
+
