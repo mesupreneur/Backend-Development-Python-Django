c7e7ec22baf2eedf34f6779469178f8b975dda01
mesupreneur <ma.mesupadhikari@gmail.com>
2023-10-20 18:27:26 +0545

    Strings in Python

---
+++ strings.py
@@ -0,0 +1,18 @@
+name = 'Mesup'
+print('Hi.\nHow are you')
+print(name)
+print("Hi.\nHow are you")
+print('Hi.\'How are you')
+print(name[0])
+#number start counting from 0
+print(name[2])
+print(name.upper())
+print(name.lower())
+#to confirm 
+name = 'MESUP'
+print(name.isupper())
+print(name.islower())
+print(name.lower().islower())
+print(len(name))
+print(name.index('M'))
+print(name.replace('M','T'))
